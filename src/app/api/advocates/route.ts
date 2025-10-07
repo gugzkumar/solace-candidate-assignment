eafbb8f1b519ff9377967f16125fd4737aa4e04e
GAGAN TUNUGUNTLA <gugzkumar@gmail.com>
2025-10-07 16:56:13 -0500

    [Feature] Improve local DB Setup Experience and Seeding

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,8 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { NextResponse } from "next/server";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
-
+export async function GET(request: NextResponse) {
+  const data = await db.select().from(advocates);
   return Response.json({ data });
 }
